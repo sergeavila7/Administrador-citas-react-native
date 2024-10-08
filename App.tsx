59304aae8ba708f0726d2139b1909ed58afc8bed
sergeavila7 <serge_avila7@yahoo.com>
2024-10-08 16:37:45 -0600

    feat: add modal and styles

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, Pressable, Modal} from 'react-native';
 
 const App = (): JSX.Element => {
-  const {title, titleBold} = styles;
+  const {container, title, titleBold, btnNewDate, btnTextNewDate} = styles;
+  const newDateHandler = () => {
+    console.log('Click');
+  };
   return (
-    <SafeAreaView>
+    <SafeAreaView style={container}>
       <Text style={title}>Administrador de Citas</Text>
       <Text style={titleBold}>Veterinaria</Text>
+      <Pressable onPress={newDateHandler} style={btnNewDate}>
+        <Text style={btnTextNewDate}>Nueva Cita</Text>
+      </Pressable>
+      <Modal animationType="slide" visible={false}>
+        <Text>Desde modal</Text>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -14,6 +23,7 @@ const App = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F3F4F6',
+    flex: 1,
   },
   title: {
     textAlign: 'center',
@@ -23,8 +33,24 @@ const styles = StyleSheet.create({
     fontWeight: '600',
   },
   titleBold: {
+    textAlign: 'center',
     fontWeight: '900',
-    color: '#6D2869',
+    fontSize: 30,
+    color: '#6D28D9',
+  },
+  btnNewDate: {
+    backgroundColor: '#6D28D9',
+    padding: 20,
+    marginTop: 20,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  btnTextNewDate: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '900',
+    textTransform: 'uppercase',
   },
 });
 
